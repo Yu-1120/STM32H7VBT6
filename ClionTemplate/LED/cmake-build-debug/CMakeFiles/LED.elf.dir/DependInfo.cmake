5019727d1b47de7fd22aebbd2a97a58db880db63
Yu-1120 <1186893249@qq.com>
2022-10-06 21:47:10 +0800

    update

---
+++ ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/startup/startup_stm32h750xx.s" "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/startup/startup_stm32h750xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32H750xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/gpio.c" "CMakeFiles/LED.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/gpio.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/main.c" "CMakeFiles/LED.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/main.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/stm32h7xx_hal_msp.c" "CMakeFiles/LED.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/stm32h7xx_it.c" "CMakeFiles/LED.elf.dir/Core/Src/stm32h7xx_it.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/syscalls.c" "CMakeFiles/LED.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Core/Src/system_stm32h7xx.c" "CMakeFiles/LED.elf.dir/Core/Src/system_stm32h7xx.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj.d"
+  "E:/GitHub/STM32H7VBT6/ClionTemplate/LED/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/LED.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # STM32H7VBT6
 反客-STM32H7VBT6-自己的一些代码
 
-TM32下载烧录事项说明：https://www.weact-tc.cn/2019/11/30/STM32Download/
+STM32下载烧录事项说明：https://www.weact-tc.cn/2019/11/30/STM32Download/
 Gitee码云资料例程固件链接：https://gitee.com/WeAct-TC/MiniSTM32H7xx.git
 Github资料例程固件链接：     https://github.com/WeActTC/MiniSTM32H7xx.git
 常用电脑软件链接： https://pan.baidu.com/s/1wPy3f_cRzPJc_YnOVKTprA 
